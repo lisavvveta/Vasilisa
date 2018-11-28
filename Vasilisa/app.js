0f91f874296e72b2d13027773188c8573a9f8805
lisavvveta <lisavvveta@mail.ru>
2018-11-28 22:48:29 +0700

    -  срач тест

--- Vasilisa/app.js
+++ Vasilisa/app.js
@@ -23,3 +23,56 @@ new Vue({
     furniture: furniture,
     furnit: furniture[0],
   }
+  
+for(var number = 0; number < h.length; number++){
+  h[number].onclick = (function(index) {
+      return function(count){
+      };
+  })(i);
+}
+
+Vue.filter('currency', function (value) {
+  return '$' + value.toFixed(2);
+});
+
+var demo = new Vue({
+  el: '#main',
+  data: {
+      services: [
+        {
+          name: 'Веб разработка',
+          price: 300,
+          active:true
+        },{
+          name: 'Дизайн',
+          price: 400,
+          active:false
+        },{
+          name: 'Интеграция',
+          price: 250,
+          active:false
+        },{
+          name: 'Обучение',
+          price: 220,
+          active:false
+        }
+      ]
+  },
+  methods: {
+    toggleActive: function(s){
+          s.active = !s.active;
+    },
+    total: function(){
+
+        var total = 0;
+
+        this.services.forEach(function(s){
+          if (s.active){
+            total+= s.price;
+          }
+        });
+
+       return total;
+      }
+  }
+});
